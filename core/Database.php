fb58854d682d9899684967121889456bf35f8cc8
Sitraka <hobynyaina.sitraka@email.com>
2026-02-19 14:28:04 +0300

    Ajout Database.php pour la connexion PDO

---
+++ core/Database.php
@@ -0,0 +1,22 @@
+<?php
+class Database
+{
+    private static ?PDO $instance = null;
+
+    public static function getConnection(): PDO
+    {
+        if (self::$instance === null) {
+            self::$instance = new PDO(
+                "mysql:host=localhost;dbname=i18n;charset=utf8mb4",
+                "root",
+                "toor",
+                [
+                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                ]
+            );
+        }
+
+        return self::$instance;
+    }
+}
